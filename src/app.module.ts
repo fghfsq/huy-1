3264bdd617f52b4cd8a21149581e35eace5a31d2
dsfs <dsfs>
2023-12-15 23:46:04 +0300

    ghj

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { TypeOrmModule } from "@nestjs/typeorm"
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module'
+import { UserEntity } from "./users/entity/user.entity"
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports:[ConfigModule.forRoot(),
@@ -12,10 +14,11 @@ import { UsersModule } from './users/users.module';
             username: process.env.USER,
             password:  process.env.PASSWORD,
             database:  process.env.DB,
-            entities: [],
+            entities: [UserEntity],
             synchronize: true,
           }),
-        UsersModule,]
+        UsersModule,
+        AuthModule,]
 })
 
 export class AppModule{}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import {Repository} from 'typeorm'
+import {InjectRepository} from '@nestjs/typeorm'
+import { UserEntity } from './entity/user.entity'
+import { CreateUserDto } from './dto/create-user.dto'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(UserEntity) private repository:Repository<UserEntity>){}
+
+    async create(dto:CreateUserDto){
+        return this.repository.create(dto)
+    }
+
+    async getUserByEmail(email:string){
+        return this.repository.findOneBy({email})
+    }
+}
