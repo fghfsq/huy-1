3e4c753b654d3cdf02001320f9034b626176a68c
dsfs <dsfs>
2023-12-15 23:34:45 +0300

    asd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,21 @@
 import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { UsersModule } from './users/users.module';
 
-@Module({})
+@Module({
+    imports:[ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.HOST,
+            port: Number(process.env.PORTDB),
+            username: process.env.USER,
+            password:  process.env.PASSWORD,
+            database:  process.env.DB,
+            entities: [],
+            synchronize: true,
+          }),
+        UsersModule,]
+})
 
 export class AppModule{}
